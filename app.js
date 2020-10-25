e82af5dee8d853e0796c7ef29c2bf6dd8a316c05
Nate Chiles <nate.chiles@gmail.com>
2020-10-25 12:16:09 -0400

    add node modules and Procfile

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var msg = "hello world"
-console.log(msg)
+const { Pool } = require('pg');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser= require('body-parser')
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, '/'))
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
